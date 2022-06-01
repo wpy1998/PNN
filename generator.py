bdafe2da4805b55466f3214d4f1275143ead15d2
wpy1998 <1251499395@qq.com>
2022-06-01 21:34:14 +0800

    commit by wpy

--- generator.py
+++ generator.py
@@ -15,30 +15,34 @@ for i in range(10):
                 result = str(input1) + ',' + str(input2) + ',' + str(input3) + ',' + str(input4) + \
                          ',' + str(input5) + ',' + str(input6) + ',' + str(input7) + ',' + str(input8) + \
                          ',' + str(input9) + ',' + str(input10) + ',' + str(input6) + ',' + str(input5) + \
-                         ',' + str(input4) + ',' + str(input3) + ',' + str(input2) + ',' + str(input1) + '\n'
+                         ',' + str(input4) + ',' + str(input3) + ',' + str(input2) + ',' + str(input1)
+                val = 0
+                if i == 9:
+                    val_1 = 1
+                    val = 1
+                else:
+                    val_1 = 0
+
+                if j == 9:
+                    val_2 = 1
+                    val = 1
+                else:
+                    val_2 = 0
+
+                if m == 9:
+                    val_3 = 1
+                    val = 1
+                else:
+                    val_3 = 0
+
+                if n == 9:
+                    val_4 = 1
+                    val = 1
+                else:
+                    val_4 = 0
+
+                code = val_1 * 8 + val_2 * 4 + val_3 * 2 + val_4
+                result = result + ',' + str(val) + ',' + str(code) + '\n'
                 with open("train_data.csv", "a") as file:
                     file.write(result)
                 print(result)
-
-                # if i == 9:
-                #     val_1 = 1
-                # else:
-                #     val_1 = 0
-                #
-                # if j == 9:
-                #     val_2 = 1
-                # else:
-                #     val_2 = 0
-                #
-                # if m == 9:
-                #     val_3 = 1
-                # else:
-                #     val_3 = 0
-                #
-                # if n == 9:
-                #     val_4 = 1
-                # else:
-                #     val_4 = 0
-                #
-                # result = val_1 * 8 + val_2 * 4 + val_3 * 2 + val_4
-                # print(result)
--- main.py
+++ main.py
@@ -2,21 +2,23 @@ import numpy as np
 import pandas as pd
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch import optim
+from torch.autograd import Variable
+from torch.utils.data import Dataset, DataLoader
 
-EPOCH = 1               # train the training data n times, to save time, we just train 1 epoch
-BATCH_SIZE = 50
+EPOCHS = 1
+batch_size = 5
 
-train_data = pd.read_csv(r'train_data.csv')
-train_data = np.array(train_data)
-x = torch.from_numpy(train_data)
-x = torch.reshape(x, (len(x), 1, 4, 4))
-x = x.to(torch.float32)
+#dataset
+class TrainDataset(Dataset):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-train_result = pd.read_csv(r'train_result.csv')
-train_result = np.array(train_result)
-y = torch.from_numpy(train_result)
+    def __getitem__(self, item):
+        return self.x[item], self.y[item]
+
+    def __len__(self):
+        return self.x.shape[0]
 
 class CNN(nn.Module):
     def __init__(self):
@@ -30,20 +32,31 @@ class CNN(nn.Module):
                 padding=1
             ),
             nn.ReLU(),
-            nn.MaxPool2d(2) # 4 * 3 * 3
+            nn.MaxPool2d(kernel_size=2)
         )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(4, 2, 3, 1, 1),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-        self.out = nn.Linear(2, 1)
+        # self.conv2 = nn.Sequential(
+        #     nn.Conv2d(4, 2, 3, 1, 1),
+        #     nn.ReLU(),
+        #     nn.MaxPool2d(2)
+        # )
+        self.out = nn.Linear(4 * 2 * 2, 4)
 
     def forward(self, x):
         x = self.conv1(x)
-        x = self.conv2(x)
+        # x = self.conv2(x)
+        x = x.view(x.size(0), -1)
         output = self.out(x)
-        return output, x
+        return output
+
+train_data = pd.read_csv(r'train_data.csv')
+x = train_data.iloc[:, : 16].to_numpy()
+x = x.reshape(len(x), 1, 4, 4)
+y = train_data['label1'].to_numpy()
+dataset = TrainDataset(x, y)
+data_loader = DataLoader(dataset, batch_size=batch_size, num_workers=0, shuffle=True, drop_last=False)
+
+# for batch_x, batch_y in data_loader:
+#     print(batch_x.shape, batch_y.shape)
 
 cnn = CNN()
 print(cnn)
@@ -51,11 +64,13 @@ print(cnn)
 optimizer = torch.optim.Adam(cnn.parameters(), lr=0.05)
 loss_func = nn.CrossEntropyLoss()
 
-for epoch in range(EPOCH):
-    for i in range(len(x)):
-        output = cnn(x[i])[0]
-        loss = loss_func(output, y[i])
+for epoch in range(5):
+    for i, (x, y) in enumerate(data_loader):
+        batch_x = Variable(x)
+        batch_y = Variable(y)
+        output = cnn(batch_x)
+        loss = loss_func(output, batch_y)
+        print('Epoch: ' + str(epoch) + ' i=' + str(i) + str(loss))
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        print(loss)
